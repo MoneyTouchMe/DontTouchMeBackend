6df92340cce01534e18ee539ebbf3a77cc1dffef
heebum kim <heebum9955@naver.com>
2025-02-17 13:58:06 +0900

    Initial Commit

---
+++ src/main/java/com/example/donttouchme/DontTouchMeApplication.java
@@ -0,0 +1,13 @@
+package com.example.donttouchme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DontTouchMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DontTouchMeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/donttouchme/DontTouchMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.donttouchme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DontTouchMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
