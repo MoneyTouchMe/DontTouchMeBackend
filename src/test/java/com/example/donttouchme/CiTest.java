330e207d984e0f2b39af396a1519b959611ed877
dowoo kim <dowoo12345@naver.com>
2025-02-17 15:38:50 +0900

    feat: CiTest 추가

---
+++ src/test/java/com/example/donttouchme/CiTest.java
@@ -0,0 +1,25 @@
+package com.example.donttouchme;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class CiTest {
+
+    @Test
+    @DisplayName("테스트 성공")
+    void test() {
+        assertThat(true).isTrue();
+    }
+
+    @Test
+    @DisplayName("테스트 실패")
+    void test2() {
+        assertThat(true).isFalse();
+    }
+}
+
