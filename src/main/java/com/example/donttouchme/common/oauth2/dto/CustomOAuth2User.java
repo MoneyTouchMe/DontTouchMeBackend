cbe726b55e91ca14d9d9f58e73ee092836c723fe
heebum kim <heebum9955@naver.com>
2025-02-19 17:17:10 +0900

    feat: OAuth2User 구현

---
+++ src/main/java/com/example/donttouchme/common/oauth2/dto/CustomOAuth2User.java
@@ -0,0 +1,42 @@
+package com.example.donttouchme.common.oauth2.dto;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
+@RequiredArgsConstructor
+public class CustomOAuth2User implements OAuth2User {
+    private final UserDto userDto;
+
+    //네이버, 구글 등 서비스 별 응답 값 형태가 서로 다르기에 획일화하기 어려워 사용하지 않을 예정
+    @Override
+    public Map<String, Object> getAttributes() {
+        return null;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+        collection.add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return userDto.role();
+            }
+        });
+
+        return collection;
+    }
+
+    @Override
+    public String getName() {
+        return userDto.name();
+    }
+
+    public String getUsername() {
+        return userDto.username();
+    }
+}
