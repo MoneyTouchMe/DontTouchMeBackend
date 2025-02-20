6c579241320d2f7c2c475f7f1f20e7a31997ba7b
dowoo kim <dowoo12345@naver.com>
2025-02-20 13:17:40 +0900

    refactor: MemberCommandService 예외처리

--- src/main/java/com/example/donttouchme/member/service/MemberCommandService.java
+++ src/main/java/com/example/donttouchme/member/service/MemberCommandService.java
@@ -20,24 +20,34 @@ public class MemberCommandService {
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public Member createMember(final CreateMemberDto createMemberDto) {
-        return memberRepository.save(Member.builderWithoutPassword()
-                .email(createMemberDto.email())
-                .name(createMemberDto.name())
-                .loginProvider(createMemberDto.loginProvider())
-                .role(createMemberDto.role())
-                .build()
-        );
+        try {
+            return memberRepository.save(Member.builderWithoutPassword()
+                    .email(createMemberDto.email())
+                    .name(createMemberDto.name())
+                    .loginProvider(createMemberDto.loginProvider())
+                    .role(createMemberDto.role())
+                    .build()
+            );
+        }catch (Exception e) {
+            throw new IllegalArgumentException("회원 생성 실패");
+        }
+
     }
 
     public Member signUp(final MemberSignUpRequest request) {
-        return memberRepository.save(
-                Member.builderWithPassword()
-                        .email(request.email())
-                        .password(bCryptPasswordEncoder.encode(request.password()))
-                        .name(request.name())
-                        .role(ROLE.user)
-                        .loginProvider(LoginProvider.original)
-                        .build()
-        );
+        try {
+            return memberRepository.save(
+                    Member.builderWithPassword()
+                            .email(request.email())
+                            .password(bCryptPasswordEncoder.encode(request.password()))
+                            .name(request.name())
+                            .role(ROLE.user)
+                            .loginProvider(LoginProvider.original)
+                            .build()
+            );
+        }catch (Exception e) {
+            throw new IllegalArgumentException("회원 생성 실패");
+        }
+
     }
 }
