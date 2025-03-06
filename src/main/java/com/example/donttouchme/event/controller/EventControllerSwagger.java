8434a8b6918912d33f55bbc1d2954d09eafa29ae
heebum kim <heebum9955@naver.com>
2025-03-06 18:45:02 +0900

    docs: Event 관련 Swagger 인터페이스 구현

---
+++ src/main/java/com/example/donttouchme/event/controller/EventControllerSwagger.java
@@ -0,0 +1,37 @@
+package com.example.donttouchme.event.controller;
+
+import com.example.donttouchme.event.controller.dto.CreateEventRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Tag(name = "Event 관련 API", description = "Event 관련 API")
+public interface EventControllerSwagger {
+
+    @Operation(
+            summary = "이벤트 생성 API",
+            description = "새로운 이벤트를 생성합니다."
+    )
+    public ResponseEntity<Long> createEvent(
+            @RequestBody @Validated final CreateEventRequest request
+    );
+
+    @Operation(
+            summary = "이벤트 삭제 API",
+            description = "이벤트를 삭제합니다.",
+            responses = {
+                    @ApiResponse(
+                            description = "성공적으로 삭제",
+                            responseCode = "204"
+                    )
+            }
+    )
+    public ResponseEntity<Void> deleteEvent(
+            @PathVariable final Long eventId
+    );
+
+}
