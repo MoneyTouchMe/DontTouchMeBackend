b9393b62697e16122b5c19387562f1b92cfc3d17
dowoo kim <dowoo12345@naver.com>
2025-03-13 16:30:44 +0900

    hotfix : MemberSignUpRequest 수정

--- src/main/java/com/example/donttouchme/member/controller/dto/MemberSignUpRequest.java
+++ src/main/java/com/example/donttouchme/member/controller/dto/MemberSignUpRequest.java
@@ -1,20 +1,24 @@
 package com.example.donttouchme.member.controller.dto;
 
-import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record MemberSignUpRequest(
         @NotBlank(message = "이름은 필수 입니다.")
         String name,
 
         @NotBlank(message = "email은 필수 입니다.")
+        @Email(message = "유효한 이메일 주소를 입력하세요.")
         String email,
 
         @NotBlank(message = "password는 필수 입니다.")
-        @Min(value = 8, message = "password는 8자 이상입니다.")
+        @Size(min = 8, message = "password는 8자 이상이어야 합니다.")
         String password,
 
         @NotBlank(message = "contact는 필수 입니다.")
+        @Pattern(regexp = "^(\\+\\d{1,3}[- ]?)?\\d{10,13}$", message = "유효한 연락처를 입력하세요.")
         String contact
 
 ) {
