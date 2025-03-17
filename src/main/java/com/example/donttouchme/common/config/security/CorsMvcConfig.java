66f385a63adbcec0b331ba7911def6b8ada49411
dowoo kim <dowoo12345@naver.com>
2025-03-17 16:36:26 +0900

    hotfix : cors 허용경로 수정

--- src/main/java/com/example/donttouchme/common/config/security/CorsMvcConfig.java
+++ src/main/java/com/example/donttouchme/common/config/security/CorsMvcConfig.java
@@ -9,10 +9,10 @@ public class CorsMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://dontouchme.vercel.app") // 허용할 출처 추가
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드 명시
-                .allowedHeaders("*") // 모든 헤더 허용
-                .exposedHeaders("Set-Cookie") // 노출할 헤더
-                .allowCredentials(true); // 쿠키/인증 정보 허용
+                .allowedOrigins("http://localhost:3000", "https://dontouchme.vercel.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS") // PATCH 추가
+                .allowedHeaders("*")
+                .exposedHeaders("Set-Cookie")
+                .allowCredentials(true);
     }
 }
