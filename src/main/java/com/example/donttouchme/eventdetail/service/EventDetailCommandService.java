50b3eed6b689df5ce20576e14be8a963822068ac
heebum kim <heebum9955@naver.com>
2025-03-17 09:57:40 +0900

    feat: 입출금 내역 삭제 기능 구현

--- src/main/java/com/example/donttouchme/eventdetail/service/EventDetailCommandService.java
+++ src/main/java/com/example/donttouchme/eventdetail/service/EventDetailCommandService.java
@@ -58,4 +58,12 @@ public class EventDetailCommandService {
         }
         return eventDetailRepository.save(eventDetail);
     }
+
+    public void deleteEventDetail(final Long eventDetailId) {
+        EventDetail findEventDetail = eventDetailRepository.findById(eventDetailId).orElseThrow(
+                () -> new IllegalArgumentException("입출금 내역 정보를 찾을 수 없습니다. eventDetailId : " + eventDetailId)
+        );
+
+        eventDetailRepository.delete(findEventDetail);
+    }
 }
