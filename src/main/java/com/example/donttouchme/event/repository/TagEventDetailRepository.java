5ff9ca1bfa6f80a3c712a546e4c185d54fa2e998
heebum kim <heebum9955@naver.com>
2025-03-19 19:42:36 +0900

    feat: TagEventDetailRepository 구현

---
+++ src/main/java/com/example/donttouchme/event/repository/TagEventDetailRepository.java
@@ -0,0 +1,7 @@
+package com.example.donttouchme.event.repository;
+
+import com.example.donttouchme.event.domain.TagEventDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TagEventDetailRepository extends JpaRepository<TagEventDetail, Long> {
+}
