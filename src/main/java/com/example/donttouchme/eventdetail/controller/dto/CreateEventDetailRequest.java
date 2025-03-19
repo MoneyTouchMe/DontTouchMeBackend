3ef9b071f3d20b67fde992fa0d9397662100f8d6
heebum kim <heebum9955@naver.com>
2025-03-19 19:38:30 +0900

    feat: CreateEventDetailRequest toEntity() 구현

--- src/main/java/com/example/donttouchme/eventdetail/controller/dto/CreateEventDetailRequest.java
+++ src/main/java/com/example/donttouchme/eventdetail/controller/dto/CreateEventDetailRequest.java
@@ -1,5 +1,8 @@
 package com.example.donttouchme.eventdetail.controller.dto;
 
+import com.example.donttouchme.event.domain.Event;
+import com.example.donttouchme.event.domain.Target;
+import com.example.donttouchme.eventdetail.domain.EventDetail;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
@@ -7,7 +10,7 @@ import java.util.List;
 
 public record CreateEventDetailRequest(
         @NotNull(message = "이벤트 고유번호는 필수입니다.")
-        long eventId,
+        Long eventId,
 
         @NotBlank(message = "입출금 분류 항목은 필수입니다.")
         String type,
@@ -26,6 +29,20 @@ public record CreateEventDetailRequest(
 
         String target,
 
+        String sendType,
+
         String contact
 ) {
+    public EventDetail toEntity(Event event, Target target) {
+        return EventDetail.builder()
+                .type(type)
+                .history(history)
+                .price(price)
+                .name(name)
+                .image(imageUrl)
+                .contact(contact)
+                .event(event)
+                .target(target)
+                .build();
+    }
 }
