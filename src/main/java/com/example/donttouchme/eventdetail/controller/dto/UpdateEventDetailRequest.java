f99e4b6b12093d0a29508f9cefefc7114ed5e707
heebum kim <heebum9955@naver.com>
2025-03-20 15:49:45 +0900

    feat: UpdateEventDetailRequest Dto 구현

---
+++ src/main/java/com/example/donttouchme/eventdetail/controller/dto/UpdateEventDetailRequest.java
@@ -0,0 +1,41 @@
+package com.example.donttouchme.eventdetail.controller.dto;
+
+import com.example.donttouchme.eventdetail.controller.dto.validation.ContactValidatable;
+import com.example.donttouchme.eventdetail.controller.dto.validation.ValidContactAndSendType;
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.List;
+
+@ValidContactAndSendType
+public record UpdateEventDetailRequest(
+        @NotBlank(message = "입출금 분류 항목은 필수입니다.")
+        String type,
+
+        @NotBlank(message = "입출금 내역명은 필수입니다.")
+        String history,
+
+        @NotBlank(message = "금액은 필수입니다.")
+        String price,
+
+        String name,
+
+        List<String> tags,
+
+        String imageUrl,
+
+        String target,
+
+        String sendType,
+
+        String contact
+) implements ContactValidatable {
+    @Override
+    public String contact() {
+        return contact;
+    }
+
+    @Override
+    public String sendType() {
+        return sendType;
+    }
+}
