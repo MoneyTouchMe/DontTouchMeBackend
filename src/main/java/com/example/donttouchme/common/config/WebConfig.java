e1043dc2d1e0f9010bdf9577233490a3276aa938
dowoo kim <dowoo12345@naver.com>
2025-04-14 18:01:05 +0900

    feat: AuthMember annotation 추가

--- src/main/java/com/example/donttouchme/common/config/WebConfig.java
+++ src/main/java/com/example/donttouchme/common/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.example.donttouchme.common.config;
 
 import com.example.donttouchme.common.config.security.AuthMemberArgumentResolver;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,9 +9,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.util.List;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthMemberArgumentResolver authMemberArgumentResolver;
+
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.add(new AuthMemberArgumentResolver());
+        resolvers.add(authMemberArgumentResolver);
     }
 }
