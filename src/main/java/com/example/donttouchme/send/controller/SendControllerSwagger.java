a254eedfe49f8534f522fafcb8a7d3a71b619919
heebum kim <heebum9955@naver.com>
2025-04-17 10:10:39 +0900

    feat: 감사장 Email 전송 기능 구현

--- src/main/java/com/example/donttouchme/send/controller/SendControllerSwagger.java
+++ src/main/java/com/example/donttouchme/send/controller/SendControllerSwagger.java
@@ -1,8 +1,12 @@
 package com.example.donttouchme.send.controller;
 
 import com.example.donttouchme.send.controller.dto.FindRecipientListResponse;
+import com.example.donttouchme.send.controller.dto.SendEmailRequest;
+import com.example.donttouchme.send.controller.dto.SendSMSRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Tag(name = "감사장 관련 API", description = "감사장 관련 API")
@@ -13,5 +17,21 @@ public interface SendControllerSwagger {
     )
     public FindRecipientListResponse findRecipientList(
             @RequestParam final Long eventId
+    );
+
+    @Operation(
+            summary = "감사장 이메일 전송 API",
+            description = "감사장을 이메일로 전송합니다."
+    )
+    public ResponseEntity<String> sendAppreciationEmail(
+            @RequestBody final SendEmailRequest request
+    );
+
+    @Operation(
+            summary = "감사장 문자 메시지 전송 API",
+            description = "감사장을 문자 메시지로 전송합니다."
+    )
+    public ResponseEntity<String> sendAppreciationSMS(
+            @RequestBody final SendSMSRequest request
     );
 }
--- src/main/java/com/example/donttouchme/send/service/SendQueryService.java
+++ src/main/java/com/example/donttouchme/send/service/SendQueryService.java
@@ -3,9 +3,11 @@ package com.example.donttouchme.send.service;
 import com.example.donttouchme.event.domain.Event;
 import com.example.donttouchme.event.repository.EventRepository;
 import com.example.donttouchme.eventdetail.domain.EventDetail;
-import com.example.donttouchme.send.controller.dto.FindRecipientListResponse;
-import com.example.donttouchme.send.controller.dto.RecipientListDto;
+import com.example.donttouchme.send.controller.dto.*;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,8 +17,10 @@ import java.util.List;
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
+@Slf4j
 public class SendQueryService {
     private final EventRepository eventRepository;
+    private final JavaMailSender mailSender;
 
     public FindRecipientListResponse findRecipientList(final Long eventId) {
         Event event = eventRepository.findById(eventId).orElseThrow(
@@ -32,5 +36,48 @@ public class SendQueryService {
             }
         }
         return new FindRecipientListResponse(dto);
+    }
+
+    public EmailSendResult sendAppreciationEmail(final SendEmailRequest request) {
+        String contentTemplate = "{name}님, 안녕하세요! 이번 {eventName}에서 뵐 수 있어 정말 반가웠습니다.\n" +
+                "귀한 시간을 내어주신 덕분에 저희에게 큰 힘이 되었습니다. 앞으로도 자주 연락드리겠습니다. 다시 한 번 감사드립니다!";
+        String titleTemplate = "{name}님, {eventName}에 참여해주셔서 감사드립니다.";
+        String eventName = request.eventName();
+        int successCnt = 0, failCnt = 0;
+
+        for (RecipientListDto recipient : request.recipients()) {
+            String to = recipient.contact();
+            String name = recipient.name();
+
+            String replaceNameC = contentTemplate.replace("{name}", name);
+            String content = replaceNameC.replace("{eventName}", eventName);
+
+            String replaceNameTt = titleTemplate.replace("{name}", name);
+            String title = replaceNameTt.replace("{eventName}", eventName);
+
+            try {
+                SimpleMailMessage email = new SimpleMailMessage();
+                email.setTo(to);
+                email.setSubject(title);
+                email.setText(content);
+                email.setFrom(request.fromEmail());
+
+                mailSender.send(email);
+                successCnt++;
+            } catch (Exception e) {
+                log.error("메일 전송 실패");
+                failCnt++;
+            }
+        }
+
+        String msg = String.format("총 %d명 중 %d명에게 메일 전송 성공, %d명 실패",
+                request.recipients().size(), successCnt, failCnt);
+
+        return new EmailSendResult(msg, successCnt, failCnt);
+    }
+
+    public String sendAppreciationSMS(final SendSMSRequest request) {
+
+        return null;
     }
 }
