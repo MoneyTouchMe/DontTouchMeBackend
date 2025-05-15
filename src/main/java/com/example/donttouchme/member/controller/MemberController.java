6e37db066d1cbeae5ef19e7c433c36dfa383d981
heebum kim <heebum9955@naver.com>
2025-05-15 09:45:26 +0900

    fix: 비밀번호 일치 확인 API HttpMethod 수정

--- src/main/java/com/example/donttouchme/member/controller/MemberController.java
+++ src/main/java/com/example/donttouchme/member/controller/MemberController.java
@@ -71,7 +71,7 @@ public class MemberController implements MemberControllerSwagger {
     }
 
     @Override
-    @PatchMapping("/withdraw")
+    @GetMapping("/check-password")
     public ResponseEntity<Void> checkCurrentPassword(
             @AuthMember Member member,
             final CheckCurrentPasswordRequest request
@@ -82,4 +82,5 @@ public class MemberController implements MemberControllerSwagger {
         }
         return ResponseEntity.noContent().build();
     }
+
 }
