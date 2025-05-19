9d86a85e829b90fcaa85b2e225d678dec27eb380
heebum kim <heebum9955@naver.com>
2025-05-19 17:24:40 +0900

    fix: ChangeMemberInfoRequest Validator 추가

--- src/main/java/com/example/donttouchme/member/controller/dto/ChangeMemberInfoRequest.java
+++ src/main/java/com/example/donttouchme/member/controller/dto/ChangeMemberInfoRequest.java
@@ -1,8 +1,13 @@
 package com.example.donttouchme.member.controller.dto;
 
+import jakarta.validation.constraints.NotNull;
+
 public record ChangeMemberInfoRequest(
+        @NotNull(message = "이름은 필수입니다.")
         String name,
+        @NotNull(message = "패스워드는 필수입니다.")
         String newPassword,
+        @NotNull(message = "연락처는 필수입니다.")
         String contact
 ) {
 }
